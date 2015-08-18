8b0c55b0846f78c432d99e16165e1634fa05063f
James Hartley <jhartley58@gmail.com>
2015-08-19 00:11:25 +0100

    Added a comment for the "randRange" method

--- src/util/MathUtils.java
+++ src/util/MathUtils.java
@@ -49,6 +49,8 @@ public class MathUtils {
 		return matrix;
 	}
 	
+	
+	// Returns uniformly distributed random doubles in a given range
 	public static double randRange(double min, double max) {
 		return min + Math.random() * (max - min);
 	}
