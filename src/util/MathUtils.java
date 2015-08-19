0601d485c28bba250c832af4e15096da08e4b7f8
James Hartley <jhartley58@gmail.com>
2015-08-19 14:22:16 +0100

    Added a comment indicating the basis vectors

--- src/util/MathUtils.java
+++ src/util/MathUtils.java
@@ -5,6 +5,7 @@ import math.Vector3f;
 
 public class MathUtils {
 	
+	// Basis vectors
 	public static final Vector3f X_AXIS = new Vector3f(1.0f, 0.0f, 0.0f);
 	public static final Vector3f Y_AXIS = new Vector3f(0.0f, 1.0f, 0.0f);
 	public static final Vector3f Z_AXIS = new Vector3f(0.0f, 0.0f, 1.0f);
