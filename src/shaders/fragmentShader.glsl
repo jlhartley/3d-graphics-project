40384a3f5c0f1c56adcd7c206ba24d864c9e74c5
James Hartley <jhartley58@gmail.com>
2015-11-17 16:34:01 +0000

    Added lighting support in the shaders

--- src/shaders/fragmentShader.glsl
+++ src/shaders/fragmentShader.glsl
@@ -1,10 +1,15 @@
 #version 400 core
 
 in vec3 pass_colour;
+in vec3 world_normal;
+in vec3 to_light;
 
 out vec4 out_color;
 
 void main()
 {
-    out_color = vec4(pass_colour, 1.0);
+    float brightness = max(dot(world_normal, to_light), 0.1);
+    
+    
+    out_color = vec4(pass_colour * brightness, 1.0);
 }
--- src/shaders/vertexShader.glsl
+++ src/shaders/vertexShader.glsl
@@ -3,29 +3,60 @@
 // Constants
 const float PI = 3.14159;
 
-// Vertex data
+// Input - vertex data
 in vec3 position;
 in vec3 normal;
 in vec3 colour;
 
-out vec3 pass_colour;
 
 // Uniforms
 uniform mat4 model_matrix;
 uniform mat4 view_matrix;
 uniform mat4 projection_matrix;
+
+uniform vec3 light_position;
+
+
+// Output
+out vec3 pass_colour;
+out vec3 to_light;
+out vec3 world_normal;
  
 //uniform vec3 colour;
 
-uniform float time;
-
 void main()
-{   
-	pass_colour = colour;
+{
+	// The w component is 0 because the normal represents a direction
+	// Normalise so scale does not affect the dot product
+	vec3 worldNormal = normalize((vec4(normal, 0) * model_matrix).xyz);
+	
+	// Alternative?
+	// vec3 worldNormal = normalize(normal * mat3(model_matrix));
+	
+	
+	// Calculate world space position
+	vec4 worldPosition = model_matrix * vec4(position, 1);
+	
+	// Difference between light position and world vertex position
+	// gives the direction vector to the light from the vertex
+	// Also normalise so scale does not affect the dot product
+	vec3 toLight = normalize(light_position - worldPosition.xyz);
+	
+	
+	
+	//pass_colour = colour;
+	
+	world_normal = worldNormal;
+	
+	to_light = toLight;
+	
 	
     //pass_colour = vec3(1,1,1);
     
-    //pass_colour = (vec4(normal, 0) * model_matrix).xyz;
+    pass_colour = normalize(vec3(abs(worldPosition.x), abs(worldPosition.y), abs(worldPosition.z)));
+    
+    
+    //pass_colour = worldNormal * 3;
     
     //pass_colour = (vec4(normalize(position), 0) * model_matrix).xyz;
     
@@ -33,5 +64,6 @@ void main()
     
     //pass_colour = mix(colour, timeBasedColour, sin(time)/2-0.5);
     
-    gl_Position = projection_matrix * view_matrix * model_matrix * vec4(position, 1);
+    
+    gl_Position = projection_matrix * view_matrix * worldPosition;
 }
