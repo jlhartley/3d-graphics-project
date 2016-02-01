517f1580f155aa0569cf9f9baf320e0da97f141d
James Hartley <jhartley58@gmail.com>
2016-02-01 22:25:40 +0000

    Continued development of timer implementation

--- src/time/Timer.java
+++ src/time/Timer.java
@@ -2,8 +2,16 @@ package time;
 
 public class Timer {
 	
-	private double time;
+	//private double time;
+	private double startTime;
 	
+	public Timer() {
+		startTime = getSystemTime();
+	}
+	
+	public double getTime() {
+		return getSystemTime() - startTime;
+	}
 	
 	public void setTime(double time) {
 		
@@ -11,6 +19,10 @@ public class Timer {
 	
 	public void zero() {
 		setTime(0);
+		startTime = getSystemTime();
 	}
 
+	private double getSystemTime() {
+		return System.nanoTime() / 10E8;
+	}
 }
