e759480b7974b9fed30dc5e5f87c3f289231c4f9
James Hartley <jhartley58@gmail.com>
2016-04-20 13:36:17 +0100

    Added skeleton for add planet dialog

---
+++ src/ui/AddPlanetDialog.java
@@ -0,0 +1,13 @@
+package ui;
+
+import org.eclipse.swt.widgets.Dialog;
+import org.eclipse.swt.widgets.Shell;
+
+public class AddPlanetDialog extends Dialog {
+
+	public AddPlanetDialog(Shell parent) {
+		super(parent);
+	}
+	
+	
+}
